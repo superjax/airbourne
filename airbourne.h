6bcb781b4b281983e851e2de68d6b9df3d23d704
James Jackson <superjax08@gmail.com>
2017-01-10 21:50:25 -0700

    removed old baseflight file

--- airbourne.h
+++ airbourne.h
@@ -1,4 +1,22 @@
 #ifndef AIRBOURNE_H
 #define AIRBOURNE_H
 
+#include "board.h"
+#include "barometer.h"
+#include "eeprom.h"
+#include "gpio.h"
+#include "hmc5883l.h"
+#include "i2c.h"
+#include "imu.h"
+#include "led.h"
+#include "magnetometer.h"
+#include "mpu6050.h"
+#include "ms5611.h"
+#include "printf.h"
+#include "pwm.h"
+#include "rc.h"
+#include "rc_dsm.h"
+#include "rc_ppm.h"
+#include "serialport.h"
+#include "uart.h"
 #endif // AIRBOURNE_H
